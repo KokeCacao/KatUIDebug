e1d3d83f5f75d2f12a65d93f2ec7f5c1d4ff0151
Hanke Chen <i@kokecacao.me>
2023-11-27 18:44:56 -0500

    :sparkles: add requirements.txt

--- debug.py
+++ debug.py
@@ -1,8 +1,11 @@
 import base64
+import io
+import imageio
+import time
+import math
 import numpy as np
 from torch import Tensor
 from PIL import Image
-from io import BytesIO
 from backend.loader.decorator import KatachiNode
 from backend.nodes.builtin import BaseNode
 
@@ -16,11 +19,10 @@ class DebugSendImageToNeRFPanel(BaseNode):
     def execute(self, image: Tensor) -> None:
         numpy_image = (image.squeeze().permute(1, 2, 0).cpu().numpy() + 1.0) * 127.5
         pil_image = Image.fromarray(np.uint8(numpy_image))
-        image_buffer = BytesIO()
+        image_buffer = io.BytesIO()
         pil_image.save(image_buffer, format='PNG')
-        image_bytes = image_buffer.getvalue()
 
-        self.set_output("render", f"data:image/png;base64,{base64.b64encode(image_bytes).decode('utf-8')}")
+        self.set_output("render", f"data:image/png;base64,{base64.b64encode(image_buffer.getvalue()).decode('utf-8')}")
         self.send_update()
         return None
 
@@ -34,4 +36,33 @@ class DebugSendError(BaseNode):
     def execute(self) -> None:
         self.set_output("error", f"This is a test error message.")
         self.send_update()
+        return None
+
+
+class DebugVideoStreamer(BaseNode):
+
+    @KatachiNode(hidden=False)
+    def __init__(self) -> None:
+        pass
+
+    def execute(self, video_path: str) -> None:
+        reader = imageio.get_reader(video_path)
+        meta_data = reader.get_meta_data()
+        fps = meta_data.get('fps', 30)
+        n_frames = meta_data.get('nframes', float('inf')) # Get the number of frames
+        print(f"Found {n_frames} frames in video {video_path}")
+
+        for step, frame in enumerate(reader.iter_data()):
+            start_time = time.time()
+            pil_img = Image.fromarray(frame)
+            image_buffer = io.BytesIO()
+            pil_img.save(image_buffer, format="PNG")
+
+            self.set_output("progress", int(100 * math.ceil(step / n_frames)))
+            self.set_output("render", f"data:image/png;base64,{base64.b64encode(image_buffer.getvalue()).decode('utf-8')}")
+            print(f"Sending frame {step} of {n_frames}")
+            self.send_update()
+
+            time.sleep(max(1.0 / fps - (time.time() - start_time), 0))
+
         return None
